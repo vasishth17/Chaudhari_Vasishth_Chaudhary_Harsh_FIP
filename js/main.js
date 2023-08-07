44766c7d5ef11d368f8ba32e6aaf52aca2835082
vasishth17 <vasishth2003@gmail.com>
2023-08-07 16:15:02 -0400

    final changes

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const videoElement = document.getElementById('#video'); // Replace 'yourVideoElementId' with the actual ID of your video element
+
+videoElement.autoplay = true;
+videoElement.load(); // Load the video
